02af2d63eacb6cdeb2649ff6f2b609305eaa4dde
davi.barbosa <davi.barbosa@in.contacnet.com.br>
2025-04-03 12:08:49 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/manual-macro-acompanhamento',
   plugins: [react(), tailwindcss()],
 })
